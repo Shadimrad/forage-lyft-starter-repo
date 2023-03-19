59b544f61acead224701bba10fe3fd61801dc9ca
potato <93452403+Shadimrad@users.noreply.github.com>
2023-03-19 16:05:48 +0530

    Update car.py

--- car.py
+++ car.py
@@ -1,10 +1,82 @@
 from abc import ABC, abstractmethod
+import datetime
 
-
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
-
+class Servicable(ABC):
     @abstractmethod
     def needs_service(self):
         pass
+
+class Engine(ABC):
+    def __init__(self):
+        pass
+    @abstractmethod
+    def needs_service(self):
+        pass
+
+class SternmanEngine(Engine):
+    # def __init__(self, last_service_date, warning_light_is_on):
+    #     super().__init__(last_service_date)
+    #     self.warning_light_is_on = warning_light_is_on
+
+    # def engine_should_be_serviced(self):
+    #     if self.warning_light_is_on:
+    #         return True
+    #     else:
+    #         return False
+
+class Battery(ABC):
+    def __init__(self):
+        pass
+    @abstractmethod
+    def needs_service(self):
+        pass
+
+class NubbinBattery(Battery):
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
+class SpindlerBattery(Battery):
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
+
+class Car(Servicable):
+    def __init__(self, engine, battery):
+        self.battery = Battery
+        self.engine = Engine
+
+    def needs_service(self):
+        pass
+class CarFactory():
+    def __init__(self):
+        pass
+    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
+        return Car(last_service_date, engine, battery)
+    
+    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
+        return Car(last_service_date, engine, battery)
+    
+    def create_palindrome(self, current_date, last_service_date, warning_light_on):
+        return Car(last_service_date, engine, battery)
+    
+    def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
+        return Car(last_service_date, engine, battery)
+    
+    def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
+        return Car(last_service_date, engine, battery)
